553172d78729437b47ebbb69a23cbcf69834048b
Zhi Lin <zhi.long.lin@gmail.com>
2020-03-23 01:17:21 -0400

    initial download

---
+++ venvmac/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zhilin/Documents/GitHub/harvard-cs50-project3/venvmac/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
