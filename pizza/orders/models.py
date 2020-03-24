29cc05d7ae5c792a06b372414ce2daf89a6b3528
Zhi Lin <zhi.long.lin@gmail.com>
2020-03-24 13:32:10 -0400

    vs code setup

--- pizza/orders/models.py
+++ pizza/orders/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     size = models.CharField(max_length=200)
-    desc = models.TextField()
+    category = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=20, decimal_places=2)
-    num_of_additions = models.IntegerField()
+    additions = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.name} ({self.size}): ${self.price}"
